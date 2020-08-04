96577e63140497b1465d8ba43632792841b28b4f
gyd <gyd0008@163.com>
2020-08-04 18:02:42 +0800

    使用redux存储轮播图数据

--- src/pages/discover/c-pages/recommend/index.js
+++ src/pages/discover/c-pages/recommend/index.js
@@ -1,18 +1,34 @@
 import React, { memo, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { getTopBannerAction } from './store/actionCreators';
 
-import request from '@/services/request';
+function YDRecommend(props) {
 
-export default memo(function YDRecommend() {
+  const { topBanners } = useSelector(state => ({
+    topBanners: state.recommend.topBanners
+  }))
+  const dispatch = useDispatch()
+
   useEffect(() => {
-    request({
-      url: '/banner'
-    }).then(res => {
-      console.log(res)
-    }, [])
-  })
+    dispatch(getTopBannerAction())
+  }, [dispatch])
+
   return (
     <div>
-      <h2>YDRecommend</h2>
+      <h2>YDRecommend: {topBanners.length}</h2>
     </div>
   )
+}
+
+/* const mapSateToProps = state => ({
+  topBanners: state.recommend.topBanners
 })
+
+const mapDispatchToProps = dispatch => ({
+  getBanners: () => {
+    dispatch(getTopBannerAction())
+  }
+}) */
+
+// export default connect(mapSateToProps, mapDispatchToProps)(memo(YDRecommend))
+export default memo(YDRecommend)
