0ab7af985c583281cbf4162b91bd813fb6e4c04b
gyd <gyd0008@163.com>
2020-08-04 16:36:20 +0800

    引入redux并组织结构

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { memo } from 'react'
+import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config'
 import { HashRouter } from 'react-router-dom'
 
+import store from './store';
 import routes from './router'
 
 import YDAppHeader from 'components/app-header'
@@ -9,10 +11,13 @@ import YDAppFooter from 'components/app-footer'
 
 export default memo(function App() {
   return (
-    <HashRouter>
-      <YDAppHeader/>
-      {renderRoutes(routes)}
-      <YDAppFooter/>
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <YDAppHeader/>
+          {renderRoutes(routes)}
+        <YDAppFooter/>
+      </HashRouter>
+    </Provider>
+    
   )
 })
--- src/pages/discover/c-pages/recommend/index.js
+++ src/pages/discover/c-pages/recommend/index.js
@@ -1,6 +1,15 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
+
+import request from '@/services/request';
 
 export default memo(function YDRecommend() {
+  useEffect(() => {
+    request({
+      url: '/banner'
+    }).then(res => {
+      console.log(res)
+    }, [])
+  })
   return (
     <div>
       <h2>YDRecommend</h2>
