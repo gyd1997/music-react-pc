405492539588edc1ec2ddd1d4951afb08b0271be
gyd1997 <gyd0008@163.com>
2020-08-16 11:31:42 +0800

    排行榜完成

--- src/pages/discover/c-pages/recommend/c-cpns/recommend-ranking/index.js
+++ src/pages/discover/c-pages/recommend/c-cpns/recommend-ranking/index.js
@@ -1,12 +1,39 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
+import { useDispatch, shallowEqual, useSelector } from "react-redux"
 
 import YDThemeHeaderRCM from '@/components/theme-header-rcm'
+import YDTopRanking from '@/components/top-ranking'
 import { RankingWrapper } from './style'
+import { getTopListAction } from "../../store/actionCreators"
 
 export default memo(function YDRecommendRanking() {
+  // redux hooks
+  const dispatch = useDispatch()
+  const { 
+    upRanking,
+    newRanking,
+    originRanking
+   } = useSelector(state =>({
+    upRanking: state.getIn(['recommend', 'upRanking']),
+    newRanking: state.getIn(['recommend', 'newRanking']),
+    originRanking: state.getIn(['recommend', 'originRanking']),
+  }), shallowEqual)
+
+  // other hooks
+  useEffect(() => {
+    dispatch(getTopListAction(0))
+    dispatch(getTopListAction(2))
+    dispatch(getTopListAction(3))
+  }, [dispatch])
+
   return (
     <RankingWrapper>
       <YDThemeHeaderRCM title="榜单"/>
+      <div className="tops">
+        <YDTopRanking info={upRanking}/>
+        <YDTopRanking info={newRanking}/>
+        <YDTopRanking info={originRanking}/>
+      </div>
     </RankingWrapper>
   )
 })
